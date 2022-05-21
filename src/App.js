902eac61cce0574aada381437890faca455e237b
rutherford-alexis <rutherford.aalexis@gmail.com>
2022-05-20 21:06:06 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Character} from 'C:/Users/ruthe/documents/web-sprint-challenge-intro-to-react/src/components/Character'
+import { data } from './mocks/handlers';
+import './App.css';
 
 const App = () => {
+
+
+  const [egg, setEgg] = useState(data);
+  const [person, setPerson]= useState('');
+
+   const name =  egg.forEach( exp => {
+        return exp.name
+  })
+  .then(()=> console.log('blue'))
+  .catch(()=> console.log('error'))
+
+  setPerson(name);
+
+
+
+  // for each character i need an unordered list with the characte name
+  // then i need to list over each part of the person object
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,9 +31,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">{person}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React, {useState} from 'react';
+
+export const Character = props => {
+
+    // i need this function to render a the data of the of a single person
+
+
+     /* const {Characters} = props;
+     const [bio, setbio] = useState('');
+     const [select, setSelect]= useState('');
+ */
+
+     const {Characters} = props;
+     
+
+ return (
+     <div>
+         <h2>{Characters.name}</h2>
+     </div>
+ )
+}
+
+/* export default Character */
